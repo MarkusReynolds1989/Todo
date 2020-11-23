0656f9ea3d4c4fb5e097af8381d15c1963e82c97
markusreynolds1989 <markusreynolds1989@gmail.com>
2020-11-23 15:11:31 -0600

    server start

--- todo/src/controlmod/todo_controller.rs
+++ todo/src/controlmod/todo_controller.rs
@@ -4,10 +4,9 @@ use std::path::Path;
 use std::io::Read;
 use serde::Serialize;
 
-pub fn load_data() -> Vec<todo_model::Task> {
+/*pub fn load_data() -> Vec<todo_model::Task> {
     let mut input = String::new();
     let path = Path::new("../storage.json");
     let mut file = File::open(file).expect("Couldn't open file.");
     file.read_to_string(&mut input); 
-    
-}
+}*/
--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,7 +1,15 @@
 use chrono::DateTime;
+use std::net::TcpListener;
 // Takes the info from the json file and gives it to the front end code.
 // This is the model and controller.
 mod controlmod;
 
 fn main() {
+   let listener = TcpListener::bind("127.0.0.1:7878").expect("Connection failed");
+   loop {
+      for stream in listener.incoming() {
+         let stream = stream.unwrap();
+         println!("Connection established");
+      }
+   }
 }
