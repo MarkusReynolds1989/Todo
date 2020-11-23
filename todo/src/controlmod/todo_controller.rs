2d9009c1eec8057e491d22ddfe3d28f6b8e7e459
markusreynolds1989 <markusreynolds1989@gmail.com>
2020-11-23 14:18:41 -0600

    Started json

--- todo/src/controlmod/todo_controller.rs
+++ todo/src/controlmod/todo_controller.rs
@@ -1,6 +1,13 @@
 use crate::controlmod::todo_model;
 use std::fs::File;
+use std::path::Path;
+use std::io::Read;
+use serde::Serialize;
 
-pub fn load_data() -> Vec<todo_model::Task>{
-    let 
+pub fn load_data() -> Vec<todo_model::Task> {
+    let mut input = String::new();
+    let path = Path::new("../storage.json");
+    let mut file = File::open(file).expect("Couldn't open file.");
+    file.read_to_string(&mut input); 
+    
 }
